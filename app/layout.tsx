641f3b29712d00ab402e8794d6a4c0e68281bef2
Valero05 <b.valero.r05@hotmail.com>
2023-11-30 14:11:18 -0300

    2nd commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 import '@/app/ui/global.css'
 import {interFont} from '@/app/ui/fonts'
 
->>>>>>> master
 export default function RootLayout({
   children,
 }: {
@@ -11,11 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-<<<<<<< HEAD
-      <body>{children}</body>
-=======
       <body className={`${interFont.className} antialised`}>{children}</body>
->>>>>>> master
     </html>
   );
 }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-=======
 'use client';
->>>>>>> master
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-<<<<<<< HEAD
-=======
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
->>>>>>> master
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -19,15 +14,6 @@ const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
-<<<<<<< HEAD
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
-  },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
-];
-
-export default function NavLinks() {
-=======
     href: '/dashboard/invoice',
     icon: DocumentDuplicateIcon,
   },
@@ -36,26 +22,22 @@ export default function NavLinks() {
 
 export default function NavLinks() {
   const usePathName = usePathname();
->>>>>>> master
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-<<<<<<< HEAD
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-=======
             className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
             {'bg-sky-100 text-blue-600':usePathName === link.href}
             )}
->>>>>>> master
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
