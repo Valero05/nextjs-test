5e2cdf6f691c2bb276a8c32c9e7c14e411753299
Valero05 <118315589+Valero05@users.noreply.github.com>
2023-11-30 14:29:29 -0300

    Update table.tsx

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -86,11 +86,11 @@ export default async function CustomersTable({
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
                           <Image
-                            alt={`${customer.name}'s profile picture`}
-                            src={customer.image_url}
-                            className="rounded-full"         
-                            width={28}
-                            height={28}
+                              alt={`${customer.name}'s profile picture`}
+                              src={customer.image_url}
+                              className="rounded-full"
+                              width={28}
+                              height={28}
                           />
                           <p>{customer.name}</p>
                         </div>
