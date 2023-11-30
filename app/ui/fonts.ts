5a7cd6207e73b87d891820631e1c4f904654d10c
Valero05 <b.valero.r05@hotmail.com>
2023-11-30 13:27:44 -0300

    ui commits

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import exp from 'constants';
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const interFont = Inter({subsets:['latin']});
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
